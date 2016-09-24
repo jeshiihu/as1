25eff8ddc872bab45d45cb538df1c0f6b0f9793e
Jessica Huynh <jhuynh@ualberta.ca>
2016-09-24 16:07:14 -0600

    adding android studio project

---
+++ app/src/main/java/com/huynh/jess/jhuynh_habittracker/HabitTrackerMainActivity.java
@@ -0,0 +1,13 @@
+package com.huynh.jess.jhuynh_habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HabitTrackerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_tracker_main);
+    }
+}
