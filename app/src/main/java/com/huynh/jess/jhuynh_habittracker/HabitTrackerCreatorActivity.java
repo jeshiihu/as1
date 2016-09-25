4e9f7ebc099778e41a63f4a85e7228ab46a40b93
Jessica Huynh <jhuynh@ualberta.ca>
2016-09-25 12:59:46 -0600

    added creation date, with auto-fill

--- app/src/main/java/com/huynh/jess/jhuynh_habittracker/HabitTrackerCreatorActivity.java
+++ app/src/main/java/com/huynh/jess/jhuynh_habittracker/HabitTrackerCreatorActivity.java
@@ -3,9 +3,13 @@ package com.huynh.jess.jhuynh_habittracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -23,8 +27,31 @@ public class HabitTrackerCreatorActivity extends AppCompatActivity
         Intent intent = getIntent();
         ViewGroup layout = (ViewGroup)findViewById(R.id.activity_habit_tracker_creator);
 
-        TextView dateText = (TextView)findViewById(R.id.textView_editorDate);
-        Date date = new Date(System.currentTimeMillis());
-        dateText.setText(date.toString());
+        // Get current date
+        Date currDate = new Date(System.currentTimeMillis());
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String strDate = format.format(currDate);
+
+        // auto fill the date edit text
+        EditText editDate = (EditText)findViewById(R.id.editText_date);
+        editDate.setText(strDate);
+
+        editDate.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 }
