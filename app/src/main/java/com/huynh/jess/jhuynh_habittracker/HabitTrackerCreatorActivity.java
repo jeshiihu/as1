9d164225482c4a5f323a57bc8ce1d80212f71071
Jessica Huynh <jhuynh@ualberta.ca>
2016-09-29 22:31:31 -0600

    implemented days on creation and gui

--- app/src/main/java/com/huynh/jess/jhuynh_habittracker/HabitTrackerCreatorActivity.java
+++ app/src/main/java/com/huynh/jess/jhuynh_habittracker/HabitTrackerCreatorActivity.java
@@ -2,11 +2,13 @@ package com.huynh.jess.jhuynh_habittracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.Touch;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,6 +24,7 @@ public class HabitTrackerCreatorActivity extends AppCompatActivity
 {
     private Habit newHabit;
     private DaysSet days = new DaysSet();
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -66,36 +69,115 @@ public class HabitTrackerCreatorActivity extends AppCompatActivity
         finish();
     }
 
-    public void onBtnClickCancel(View view) { // return from activity
+    public void onBtnClickCancel(View view)
+    { // return from activity
         setResult(RESULT_CANCELED);
         finish();
     }
 
-    public void onBtnClickSun(View view) {
-       // change colour
+
+    public void onCtnClickMon(View view)
+    {
+        Button btnMon = (Button)findViewById(R.id.btn_monday);
+
+        if(days.isDayContained(DaysSet.Day.Monday))
+        {
+            btnMon.setBackgroundColor(ContextCompat.getColor(this, R.color.gray));
+            days.removeDay(DaysSet.Day.Monday);
+        }
+        else {
+            btnMon.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow));
+            days.addDay(DaysSet.Day.Monday);
+        }
     }
 
-    public void onBtnClickMon(View view) {
-        //change colour
+    public void onBtnClickTue(View view)
+    {
+        Button btnTue = (Button)findViewById(R.id.btn_tuesday);
+
+        if(days.isDayContained(DaysSet.Day.Tuesday))
+        {
+            btnTue.setBackgroundColor(ContextCompat.getColor(this, R.color.gray));
+            days.removeDay(DaysSet.Day.Tuesday);
+        }
+        else {
+            btnTue.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow));
+            days.addDay(DaysSet.Day.Tuesday);
+        }
     }
 
-    public void onBtnClickTue(View view) {
-        // change colour
+    public void onBtnClickWed(View view)
+    {
+        Button btnWed = (Button)findViewById(R.id.btn_wednesday);
+
+        if(days.isDayContained(DaysSet.Day.Wednesday))
+        {
+            btnWed.setBackgroundColor(ContextCompat.getColor(this, R.color.gray));
+            days.removeDay(DaysSet.Day.Wednesday);
+        }
+        else {
+            btnWed.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow));
+            days.addDay(DaysSet.Day.Wednesday);
+        }
     }
 
-    public void onBtnClickWed(View view) {
-        //change colour
+    public void onBtnClickThu(View view)
+    {
+        Button btnThu = (Button)findViewById(R.id.btn_thursday);
+
+        if(days.isDayContained(DaysSet.Day.Thursday))
+        {
+            btnThu.setBackgroundColor(ContextCompat.getColor(this, R.color.gray));
+            days.removeDay(DaysSet.Day.Thursday);
+        }
+        else {
+            btnThu.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow));
+            days.addDay(DaysSet.Day.Thursday);
+        }
     }
 
-    public void onBtnClickThu(View view) {
-        // change colour
+    public void onBtnClickFri(View view)
+    {
+        Button btnFri = (Button)findViewById(R.id.btn_friday);
+
+        if(days.isDayContained(DaysSet.Day.Friday))
+        {
+            btnFri.setBackgroundColor(ContextCompat.getColor(this, R.color.gray));
+            days.removeDay(DaysSet.Day.Friday);
+        }
+        else {
+            btnFri.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow));
+            days.addDay(DaysSet.Day.Friday);
+        }
     }
 
-    public void onBtnClickFri(View view) {
-        //change colour
+    public void onBtnClickSat(View view)
+    {
+        Button btnSat = (Button)findViewById(R.id.btn_saturday);
+
+        if(days.isDayContained(DaysSet.Day.Saturday))
+        {
+            btnSat.setBackgroundColor(ContextCompat.getColor(this, R.color.gray));
+            days.removeDay(DaysSet.Day.Saturday);
+        }
+        else {
+            btnSat.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow));
+            days.addDay(DaysSet.Day.Saturday);
+        }
     }
 
-    public void onBtnClickSat(View view) {
-        // change colour
+    public void onBtnClickSun(View view)
+    {
+        Button btnSun = (Button)findViewById(R.id.btn_sunday);
+
+        if(days.isDayContained(DaysSet.Day.Sunday))
+        {
+            btnSun.setBackgroundColor(ContextCompat.getColor(this, R.color.gray));
+            days.removeDay(DaysSet.Day.Sunday);
+        }
+        else {
+            btnSun.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow));
+            days.addDay(DaysSet.Day.Sunday);
+        }
     }
 }
