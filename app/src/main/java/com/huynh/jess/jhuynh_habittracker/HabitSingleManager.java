1193cc8b272863cfa17c7839fd51ada83a443263
Jessica Huynh <jhuynh@ualberta.ca>
2016-09-30 01:36:48 -0600

    adding SingleView manager and tests

---
+++ app/src/main/java/com/huynh/jess/jhuynh_habittracker/HabitSingleManager.java
@@ -0,0 +1,14 @@
+package com.huynh.jess.jhuynh_habittracker;
+
+/**
+ * Created by Jess on 2016-09-30.
+ */
+public class HabitSingleManager
+{
+    CompletedHabit completedHabit;
+
+    public void HabitSingleManager()
+    {
+
+    }
+}
---
+++ app/src/test/java/com/huynh/jess/jhuynh_habittracker/HabitSingleManagerTest.java
@@ -0,0 +1,23 @@
+package com.huynh.jess.jhuynh_habittracker;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jess on 2016-09-30.
+ */
+
+//@Test
+//public void ()
+//{
+//
+//}
+
+public class HabitSingleManagerTest
+{
+    @Test
+    public void dailyCompletion()
+    {
+        HabitSingleManager manager = new HabitSingleManager();
+    }
+}
