7e350a0c28fe0398a831f0846c299f40e1e8eb7b
Jessica Huynh <jhuynh@ualberta.ca>
2016-09-30 00:52:35 -0600

    fixed up edit view gui

--- app/src/main/java/com/huynh/jess/jhuynh_habittracker/HabitTrackerEditActivity.java
+++ app/src/main/java/com/huynh/jess/jhuynh_habittracker/HabitTrackerEditActivity.java
@@ -2,14 +2,13 @@ package com.huynh.jess.jhuynh_habittracker;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.PopupWindow;
 
 /**
  * Created by Jess on 2016-09-25.
@@ -20,31 +19,27 @@ public class HabitTrackerEditActivity extends AppCompatActivity
     private final String DELETE_ACTION = "delete";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_habit_tracker_editor);
-
-        ImageButton deleteBtn = (ImageButton)findViewById(R.id.btn_deleteHabit);
-        deleteBtn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                DeleteWarning();
-            }
-        });
-
-        ImageButton completeBtn = (ImageButton)findViewById(R.id.btn_completeHabit);
-        completeBtn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-            }
-        });
+        setContentView(R.layout.activity_habit_tracker_single);
     }
 
-    private void DeleteWarning()
+    private void leaveView(String buttonPressed)
+    {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("actionType", buttonPressed);
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
+
+    public void onBtnClickComplete(View view)
+    {
+        ImageButton completeBtn = (ImageButton)findViewById(R.id.btn_completeHabit);
+        completeBtn.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.btn_complete_green));
+    }
+
+    public void onBtnClickDelete(View view)
     {
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(HabitTrackerEditActivity.this);
         alertBuilder.setMessage("Are you sure you want to delete this habit?");
@@ -71,11 +66,4 @@ public class HabitTrackerEditActivity extends AppCompatActivity
         deleteAlert.show();
     }
 
-    private void leaveView(String buttonPressed)
-    {
-        Intent returnIntent = new Intent();
-        returnIntent.putExtra("actionType", buttonPressed);
-        setResult(RESULT_OK, returnIntent);
-        finish();
-    }
 }
